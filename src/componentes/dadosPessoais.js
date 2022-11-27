08e0796f00e5ca2ab0d950e21850f50d708b76aa
Alexsandrab <sandrinhaleto@hotmail.com>
2022-11-26 23:32:13 -0300

    atualizando app

--- src/componentes/dadosPessoais.js
+++ src/componentes/dadosPessoais.js
@@ -1,25 +1,25 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, TouchableOpacity, View} from "react-native";
+import { StyleSheet, View} from "react-native";
 import { Button, Text, ThemeProvider } from '@rneui/themed';
 import api from "../services/api.js";
 
 export default function DadosPessoais({navigation}){
+    //tentei fazer essa funcao e depois chamar um botão, mas sem sucesso, ele não conseguiu achar a rota
+    async function consulta(){
+        const response = await api.get('dados');
+        alert(response);
+    }   
 
-    async function consultando() {
-
-        const response = await api.get('/dadosPessoais');
-        console.log(response)
-        
-    }
     return(
         <View style = {styles.principal}>
         <Text>
+        <Text style = {styles.h1Style}>{`RUTE BEATRIZ DA SILVA \n`}</Text>
         <Text style = {styles.titulo}>{`Endereço \n`}</Text>
-        <Text style = {styles.linhas}> {}</Text>
+        <Text style = {styles.linhas}> {`Rua da Aurora - 14 - Paulista - PE \n`}</Text>
         <Text style = {styles.titulo}>{`Telefone \n`}</Text>
-        <Text style = {styles.linhas}>{}</Text>
+        <Text style = {styles.linhas}>{`81 - 981143296 \n`}</Text>
         <Text style = {styles.titulo}>{`Email \n`}</Text>
-        <Text style = {styles.linhas}>{}</Text>
+        <Text style = {styles.linhas}>{`Rute@gmail.com \n`}</Text>
         </Text>
         <Button
         size="lg"
